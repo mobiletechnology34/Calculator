a586d0b0f9ebee44f1d5c38a81a466c413eabcd9
mobiletechnology34 <46763915+mobiletechnology34@users.noreply.github.com>
2019-01-19 16:38:05 -0500

    delete multi method

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,7 @@ public class Calculator {
     public static void main(String[] args) {
 
         System.out.println(add(5.3,9.7));
+       
     }
 
     public static double add(double num1,double num2){
@@ -14,9 +15,6 @@ public class Calculator {
 
         return num1/num2;
     }
-    public static double multi(double num1,double num2){
-        return num1*num2;
-    }
-
+  
 
 }
